b7bb589ba2e5df859822606d692122bebf9d340a
covasedu <eduardocovas@outlook.com>
2024-11-15 14:10:22 -0300

    skill issue

--- Legacy-WiFi-Patcher-sequoia/Shims/CoreWiFi/Main.m
+++
@@ -1,9 +0,0 @@
-#import "Utils.h"
-
-BOOL _CSDeviceSupportsAirDrop();
-BOOL fake__CSDeviceSupportsAirDrop()
-{
-	return true;
-}
-
-DYLD_INTERPOSE(fake__CSDeviceSupportsAirDrop,_CSDeviceSupportsAirDrop)
